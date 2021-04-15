00b5e3f3f7d815a2529c5869cb15a7751434810f
Rati1324 <rati.mtsituri@gau.edu.ge>
2021-04-15 18:24:21 +0400

    vector task added

--- hw/week_4/7.php
+++ hw/week_4/7.php
@@ -1,5 +1,15 @@
-
 <?php
+    function create_vector($x, $n){
+        if ($n == 0) {
+            array_push($x, [rand(0,10)]);
+            return $x;
+        }
+        $res = [];
+        for ($i = 0; $i < $n; $i++){
+            array_push($res, create_vector($x, $n-1));
+        }
+        return $res;
+    }
 
-    
+    echo(create_vector([], 3));
 ?>
