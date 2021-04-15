e5b11dca5c58014a4d8531dfcf3819417e152200
Rati1324 <rati.mtsituri@gau.edu.ge>
2021-04-15 19:16:11 +0400

    fixed vector

--- hw/week_4/7.php
+++ hw/week_4/7.php
@@ -1,15 +1,16 @@
 <?php
-    function create_vector($x, $n){
-        if ($n == 0) {
-            array_push($x, [rand(0,10)]);
+    // num_of_elements aris titoeul vectorshi elementebis raodenoba
+    function create_vector($x, $dimension, $num_of_elements){
+        if ($dimension == 0) {
+            for ($j = 0; $j < $num_of_elements; $j++){
+                array_push($x, [rand(0,10)]);
+            }
             return $x;
         }
         $res = [];
-        for ($i = 0; $i < $n; $i++){
-            array_push($res, create_vector($x, $n-1));
-        }
+        array_push($res, create_vector($x, $dimension-1, $num_of_elements));
         return $res;
     }
 
-    echo(create_vector([], 3));
+    print_r(create_vector([], 2, 4));
 ?>
