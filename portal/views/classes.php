6d027ab80f759c5ab8513290d5caea9fa03f5342
Rati1324 <rati.mtsituri@gau.edu.ge>
2021-05-21 22:12:44 +0400

    class groups

--- portal/views/classes.php
+++ portal/views/classes.php
@@ -7,108 +7,112 @@ if (isset($_SESSION['email'])) {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../static/classes.css">
-    <link rel="stylesheet" href="../static/layout.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="../static/classes.css">
+        <link rel="stylesheet" href="../static/layout.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    </head>
 
 
-<body>
-    <div class="outer_container">
-        <div class="header_and_content">
-            <?php include('./partials/header.php') ?>
+    <body>
+        <div class="outer_container">
+            <div class="header_and_content">
+                <?php include('./partials/header.php') ?>
 
-            <div class="content">
-                <?php include('./partials/sidebar.php') ?>
+                <div class="content">
+                    <?php include('./partials/sidebar.php') ?>
 
-                <div class="info">
-                    <div class="search_wrapper">
-                        <input type="text" class="search" id="search">
-                        <span>Search by: </span>
-                        <select name="" id="search_by">
-                            <option value="Title">Title</option>
-                            <option value="Lecturer">Lecturer</option>
-                            <option value="Credits">Credits</option>
-                            <option value="Time">Time</option>
-                            <option value="Code">Code</option>
-                        </select>
-                        <button class="search_btn" id="search_btn">Search</button>
-                    </div>
+                    <div class="info">
+                        <div class="search_wrapper">
+                            <input type="text" class="search" id="search">
+                            <span>Search by: </span>
+                            <select name="" id="search_by">
+                                <option value="Title">Title</option>
+                                <option value="Lecturer">Lecturer</option>
+                                <option value="Credits">Credits</option>
+                                <option value="Time">Time</option>
+                                <option value="Code">Code</option>
+                            </select>
+                            <button class="search_btn" id="search_btn">Search</button>
+                        </div>
 
-                    <table>
-                        <thead>
-                            <tr>
-                                <td style="width:30%"> Name </td>
-                                <td style="width:20%"> Lecturer </td>
-                                <td> Credit </td>
-                                <td> Code </td>
-                            </tr>
-                        </thead>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td style="width:30%"> Name </td>
+                                    <td style="width:20%"> Lecturer </td>
+                                    <td> Credit </td>
+                                    <td> Code </td>
+                                </tr>
+                            </thead>
 
-                        <tbody>
-                            <?php
-                            
-                            foreach ($classes as $c) {
-                                echo "<tr>";
-                                foreach ($c as $k => $v) {
-                                    if ($k == "class_id") { ?>
-                                        <td> <button id="<?= $v ?>" class='group_appear'> Groups </button> </td>
+                            <tbody>
+                                <?php
 
-                                        <tr class="group_head" name="group_head_<?= $v ?>" style="display:none">
-                                            <td>Day</td>
-                                            <td>Time</td>
-                                            <td>Group</td>
-                                            <td>Room</td>
-                                        </tr>
-
-                                        <?php foreach ($groups[$v] as $g) { ?>
-                                            <tr class="group" name="group_<?= $v ?>" style="display:none">
-                                                <td><?= $g['day'] ?></td>
-                                                <td><?= $g['time'] ?></td>
-                                                <td><?= $g['group'] ?></td>
-                                                <td><?= $g['roomd'] ?></td>
-                                            </tr>
-                                        <?php }
-                                        } else
+                                foreach ($classes as $c) {
+                                    $class_id = $c['class_id'];
+                                    echo "<tr>";
+                                    foreach ($c as $k => $v) {
+                                        if ($k != "class_id")
                                             echo "<td class='class_info'>$v</td>";
+                                    }?>
+                                        <td> 
+                                            <button id=<?=$class_id?> class='group_appear'> 
+                                                View groups 
+                                            </button> 
+                                        </td>
+                                    </tr>
+
+                                    <tr name='group_head_<?=$class_id?>' style='display:none'>
+                                        <td>Day</td>
+                                        <td>Time</td>
+                                        <td>Group</td>
+                                        <td>Room</td>
+                                    </tr>
+
+                                    <?php
+                                    foreach($groups[$class_id] as $g){
+                                        echo "<tr name='group_$class_id' style='display:none'>";
+                                        foreach($g as $i){
+                                            echo "<td>$i</td>";
+                                        }
+                                        echo "</tr>";
+                                    }
                                 }
-                                echo "</tr>";
-                            }
-                            ?>
-                        </tbody>
-                    </table>
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <script>
-        var buttons = document.querySelectorAll(".group_appear");
-        buttons.forEach((b) => {
-            b.addEventListener('click', () => {
-                if (document.getElementsByName("group_head_" + b.id)[0].style.display != 'table-row') {
-                    var elems = document.getElementsByName("group_" + b.id);
-                    elems.forEach((x) => x.style.display = 'table-row');
-                    document.getElementsByName("group_head_" + b.id)[0].style.display = 'table-row';
-                } else {
-                    var elems = document.getElementsByName("group_" + b.id);
-                    elems.forEach((x) => x.style.display = 'none');
-                    document.getElementsByName("group_head_" + b.id)[0].style.display = 'none';
-                }
+        <script>
+            var buttons = document.querySelectorAll(".group_appear");
+            buttons.forEach((b) => {
+                b.addEventListener('click', () => {
+                    if (document.getElementsByName("group_head_" + b.id)[0].style.display != 'table-row') {
+                        var elems = document.getElementsByName("group_" + b.id);
+                        elems.forEach((x) => x.style.display = 'table-row');
+                        document.getElementsByName("group_head_" + b.id)[0].style.display = 'table-row';
+                    } else {
+                        var elems = document.getElementsByName("group_" + b.id);
+                        elems.forEach((x) => x.style.display = 'none');
+                        document.getElementsByName("group_head_" + b.id)[0].style.display = 'none';
+                    }
+                })
             })
-        })
-    </script>
-    <?php include('./partials/footer.php') ?>
+        </script>
+        <?php include('./partials/footer.php') ?>
 
-</body>
+    </body>
 
-</html>
+    </html>
 
 <?php } ?>
